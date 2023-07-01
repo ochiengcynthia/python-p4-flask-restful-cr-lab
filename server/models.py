10a115b1344d5abbfc6f0e64f6605807ea3bd0c7
ochiengcynthia <126596182+ochiengcynthia@users.noreply.github.com>
2023-07-02 01:33:17 +0300

    completed

--- server/models.py
+++ server/models.py
@@ -5,5 +5,11 @@ db = SQLAlchemy()
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    image = db.Column(db.String(255))
+    price = db.Column(db.Float)
+
+
 
     id = db.Column(db.Integer, primary_key=True)
